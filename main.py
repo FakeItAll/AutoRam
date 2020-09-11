fb9b0c609dd74ebdf8868ce97a937284f729bab4
Andrews <adegtyarev99@gmail.com>
2020-09-11 14:18:55 +0300

    Доработана визуальная часть

--- main.py
+++ main.py
@@ -1,53 +1,6 @@
 from loader import Loader
 from collect_manger import CollectManager
-import tkinter as tk
-
-
-class MainWindow:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.canvas = tk.Canvas(self.root, width=200, height=200, bg='white')
-        self.canvas.pack()
-        self.arrow_first, self.arrow_last = tk.FIRST, tk.LAST
-        self.arrow_length = 20
-        self.text_center = tk.CENTER
-
-    def gen_coords(self):
-        return [20, 50, 180, 100]
-
-    def draw_schema(self, schema):
-        rect_coords = self.gen_coords()
-        width = abs(rect_coords[0] - rect_coords[2])
-        height = abs(rect_coords[1] - rect_coords[3])
-
-        if schema.io_settings.get('count'):
-            in_count, out_count = schema.io_settings['count']
-        else:
-            in_count, out_count = 3, 7
-
-        self.canvas.create_rectangle(*rect_coords)
-
-        rect_center_x = min(rect_coords[0], rect_coords[2]) + width // 2
-        rect_center_y = min(rect_coords[1], rect_coords[3]) + height // 2
-        self.canvas.create_text(rect_center_x, rect_center_y,
-                                text=schema.code, justify=self.text_center, font='Arial 14')
-
-        shift = width // (in_count + 1)
-        for i in range(in_count):
-            cur_x = rect_coords[0] + shift * (i + 1)
-            from_y = rect_coords[3]
-            to_y = rect_coords[3] + self.arrow_length
-            self.canvas.create_line(cur_x, from_y, cur_x, to_y, arrow=self.arrow_first)
-
-        shift = width // (out_count + 1)
-        for i in range(out_count):
-            cur_x = rect_coords[0] + shift * (i + 1)
-            from_y = rect_coords[1]
-            to_y = rect_coords[1] - self.arrow_length
-            self.canvas.create_line(cur_x, from_y, cur_x, to_y, arrow=self.arrow_last)
-
-    def execute(self):
-        self.root.mainloop()
+from mainwindow import MainWindow
 
 
 def main():
@@ -65,12 +18,12 @@ def main():
     uid3 = cm.add(mod3)
     cm.connect({uid2: {'D0': [uid3, 'I1'], 'D1': [uid3, 'I2']}})
 
-    mw = MainWindow()
-    mw.draw_schema(mod1)
-    mw.execute()
-
     ins = {'A': 1, 'B': 0, 'C': 0}
     cm.execute(uid1, ins, True)
+
+    mw = MainWindow()
+    mw.draw_schema(mod1, [360, 60])
+    mw.execute()
 
 
 if __name__ == '__main__':
---
+++ mainwindow.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+
+
+class MainWindow(object):
+    def __init__(self):
+        root = tk.Tk()
+        root.title('AutoRAM')
+        root.geometry('1200x400+100+100')
+        # root.state('zoomed')
+        self.root = root
+
+        root.update_idletasks()
+        width = root.winfo_width()
+        height = root.winfo_height()
+
+        canvas_width = width // 2
+        canvas_height = height - 10
+        canvas = tk.Canvas(self.root,
+                           width=canvas_width, height=canvas_height, bg='lightgrey')
+        canvas.pack(side='left')
+        self.canvas = canvas
+
+        self.consts = {'center': tk.CENTER, 'first': tk.FIRST, 'last': tk.LAST, 'arrow_length': 20}
+
+    def draw_schema(self, schema, rect_coords=[]):
+        in_count, out_count = schema.io_settings['count']
+
+        width = max(in_count, out_count) * 20
+        height = 40
+
+        rect_coords.append(rect_coords[0] + width)
+        rect_coords.append(rect_coords[1] + height)
+
+        self.canvas.create_rectangle(*rect_coords)
+
+        rect_center_x = min(rect_coords[0], rect_coords[2]) + width // 2
+        rect_center_y = min(rect_coords[1], rect_coords[3]) + height // 2
+        self.canvas.create_text(rect_center_x, rect_center_y,
+                                text=schema.code, justify=self.consts['center'], font='Arial 14')
+
+        shift = width // (in_count + 1)
+        for i in range(in_count):
+            cur_x = rect_coords[0] + shift * (i + 1)
+            from_y = rect_coords[3]
+            to_y = rect_coords[3] + self.consts['arrow_length']
+            self.canvas.create_line(cur_x, from_y, cur_x, to_y, arrow=self.consts['first'])
+
+        shift = width // (out_count + 1)
+        for i in range(out_count):
+            cur_x = rect_coords[0] + shift * (i + 1)
+            from_y = rect_coords[1]
+            to_y = rect_coords[1] - self.consts['arrow_length']
+            self.canvas.create_line(cur_x, from_y, cur_x, to_y, arrow=self.consts['last'])
+
+        return schema.uid
+
+    def execute(self):
+        self.root.mainloop()
