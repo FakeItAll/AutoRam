2da13855708a09d27671b1ab4d7f11d00cb43a81
Xion-Rassl <ruslan_home@list.ru>
2020-09-12 03:32:32 +0300

    Визуальные изменения отрисовки схемы и матрицы

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     mod1 = loader.load_schema('decryptor')
     mod2 = loader.load_schema('multiplexer')
     mod3 = loader.load_schema('and')
+    mod4 = loader.load_schema('storage_matrix')
 
     mod1.connector.add(mod2, {'Y0': 'A1', 'Y1': 'A2', 'Y2': 'X0'})
     mod2.connector.add(mod3, {'Y': 'I1', 'Y': 'I2'})
@@ -17,14 +18,16 @@ def main():
     cm.add(mod1)
     cm.add(mod2)
     cm.add(mod3)
+    cm.add(mod4)
 
     ins = {'A1': 1, 'A2': 0, 'A3': 0}
     cm.execute(mod1, ins, True)
 
     mw = MainWindow()
-    mw.draw_schema(mod1, [100, 300])
-    mw.draw_schema(mod2, [50, 150])
-    mw.draw_schema(mod3, [60, 40])
+    mw.draw_schema(mod1, [50, 50])
+    mw.draw_schema(mod2, [200, 50])
+    mw.draw_schema(mod3, [350, 50])
+    mw.draw_matrix(mod4, [500, 50])
     mw.execute()
 
 "Тест"
--- mainwindow.py
+++ mainwindow.py
@@ -5,7 +5,7 @@ class MainWindow(object):
     def __init__(self):
         root = tk.Tk()
         root.title('AutoRAM')
-        root.geometry('1000x400+100+100')
+        root.geometry('1200x400+100+100')
         # root.state('zoomed')
         self.root = root
 
@@ -13,7 +13,7 @@ class MainWindow(object):
         width = root.winfo_width()
         height = root.winfo_height()
 
-        canvas_width = width // 2
+        canvas_width = width - width // 3
         canvas_height = height - 10
         canvas = tk.Canvas(self.root,
                            width=canvas_width, height=canvas_height, bg='lightgrey')
@@ -29,38 +29,75 @@ class MainWindow(object):
         else:
             in_count, out_count = schema.io_settings['count']
 
-        width = max(in_count, out_count) * 25
-        height = 60
+        width = 90
+        height = max(in_count, out_count) * 18
 
         rect_coords.append(rect_coords[0] + width)
         rect_coords.append(rect_coords[1] + height)
 
         self.canvas.create_rectangle(*rect_coords)
 
-        rect_center_x = min(rect_coords[0], rect_coords[2]) + width // 2
-        rect_center_y = min(rect_coords[1], rect_coords[3]) + height // 2
+        rect_center_x = rect_coords[0] + width // 2
+        rect_center_y = rect_coords[1] + height // 2
         self.canvas.create_text(rect_center_x, rect_center_y,
                                 text=schema.code, justify=self.consts['center'], font='Arial 14')
 
-        shift = width // (in_count + 1)
+        shift = height // (in_count + 1)
         for i in range(in_count):
-            cur_x = rect_coords[0] + shift * (i + 1)
-            from_y = rect_coords[3]
-            to_y = rect_coords[3] + self.consts['arrow_length']
-            self.canvas.create_line(cur_x, from_y, cur_x, to_y, arrow=self.consts['first'])
-            self.canvas.create_text(cur_x, from_y - 8,
+            cur_y = rect_coords[1] + shift * (i + 1)
+            from_x = rect_coords[0] - self.consts['arrow_length']
+            to_x = rect_coords[0]
+            self.canvas.create_line(from_x, cur_y, to_x, cur_y, arrow=self.consts['last'])
+            self.canvas.create_text(to_x + 4 * len(in_names[i]), cur_y,
                                     text=in_names[i], justify=self.consts['center'], font='Arial 8')
 
-        shift = width // (out_count + 1)
+        shift = height // (out_count + 1)
         for i in range(out_count):
-            cur_x = rect_coords[0] + shift * (i + 1)
-            from_y = rect_coords[1]
-            to_y = rect_coords[1] - self.consts['arrow_length']
-            self.canvas.create_line(cur_x, from_y, cur_x, to_y, arrow=self.consts['last'])
-            self.canvas.create_text(cur_x, from_y + 8,
+            cur_y = rect_coords[1] + shift * (i + 1)
+            from_x = rect_coords[2]
+            to_x = rect_coords[2] + self.consts['arrow_length']
+            self.canvas.create_line(from_x, cur_y, to_x, cur_y, arrow=self.consts['last'])
+            self.canvas.create_text(from_x - 4 * len(out_names[i]), cur_y,
                                     text=out_names[i], justify=self.consts['center'], font='Arial 8')
 
         return schema.uid
+
+    def draw_matrix(self, matrix, rect_coords=[]):
+        if matrix.io_settings.get('names'):
+            in_names, out_names = matrix.io_settings['names']
+            in_count, out_count = len(in_names), len(out_names)
+        else:
+            in_count, out_count = matrix.io_settings['count']
+
+        width = out_count * 18
+        height = in_count * 18
+
+        rect_coords.append(rect_coords[0] + width)
+        rect_coords.append(rect_coords[1] + height)
+
+        self.canvas.create_rectangle(*rect_coords)
+
+        shift = height // (in_count + 1)
+        from_x = rect_coords[0] - self.consts['arrow_length']
+        to_x = rect_coords[0]
+        from_x2 = rect_coords[2] - width // (out_count + 1)
+        for i in range(in_count):
+            cur_y = rect_coords[1] + shift * (i + 1)
+            self.canvas.create_line(from_x, cur_y, to_x, cur_y, arrow=self.consts['last'])
+            self.canvas.create_line(from_x2, cur_y, to_x, cur_y)
+
+        shift = width // (out_count + 1)
+        from_y = rect_coords[3]
+        to_y = rect_coords[3] + self.consts['arrow_length']
+        to_y2 = rect_coords[1] + height // (in_count + 1)
+        for i in range(out_count):
+            cur_x = rect_coords[0] + shift * (i + 1)
+            self.canvas.create_line(cur_x, from_y, cur_x, to_y, arrow=self.consts['last'])
+            self.canvas.create_line(cur_x, from_y, cur_x, to_y2)
+
+        x0 = rect_coords[0] + height // (in_count + 1)*2
+        y0 = rect_coords[1] + width // (out_count + 1)*2
+        self.canvas.create_oval(x0-3, y0-3, x0+3, y0+3, fill='blue')
 
     def execute(self):
         self.root.mainloop()
